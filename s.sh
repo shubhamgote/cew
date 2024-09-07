b6aee91e5d4852891e8313b04ddc9c61aeabef8d
Shubham Gote <0shubhamgote0@gmail.com>
2024-09-07 16:51:50 +0530

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,3 +1,23 @@
-pkg update & upgrade
-termux-setup-storage
-pkg install openssl openssh
+#!/data/data/com.termux/files/usr/bin/bash
+
+# banner
+echo "========================================================="
+echo -e "\033[33m                          ___                           \033[0m"
+echo -e "\033[33m | | |_      ._ _|_        |  ._   _ _|_  _. | |  _  ._ \033[0m"
+echo -e "\033[33m |_| |_) |_| | | |_ |_|   _|_ | | _>  |_ (_| | | (/_ |  \033[0m"
+echo -e "\033[33m                                                        \033[0m"
+echo "========================================================="
+echo -e "\033[33m---- Written by dmdhrumilmistry ----\033[0m"
+echo "========================================================="
+echo -e "\033[33m[*] Script downloaded from https://github.com/dmdhrumilmistry/TermuxScripts\033[0m"
+# end banner
+
+# start installation
+echo -e "\033[33m[*] Updating packages...\033[0m"
+pkg update -y
+
+echo -e "\033[33m[*] Upgrading packages...\033[0m"
+pkg upgrade -y
+
+echo -e "\033[33m[*] Installing requirements...\033[0m"
+pkg install proot-distro -y
